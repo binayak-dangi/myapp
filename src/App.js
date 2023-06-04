4dcc7de42adb411dada3f5b1e02a1c1a17de5e94
binayak-dangi <binayakdangi@gmail.com>
2023-06-04 22:44:03 +0545

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import First from './First';
+
+const App=()=> {
+  return (
+    <div className="App">
+      <h2>Binayak Dangi</h2>
+      <hr />
+      <First/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/First.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const First = () => {
+    return (
+    < >
+    <h1>This is the first component</h1>
+    <h1>we are using functional component</h1>
+    </>
+    )
+}
+
+export default First
